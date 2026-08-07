9e00eb8ea9765681a4d509146ca625a01de700ce
Azalea <noreply@aza.moe>
2026-08-07 19:51:45 -0400

    [+] Create repo

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(RestoryQOL.Core), "RestoryQOL", "1.0.0", "Azalea", null)]
+[assembly: MelonGame("Mandragora", "Restory")]
+
+namespace RestoryQOL
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
